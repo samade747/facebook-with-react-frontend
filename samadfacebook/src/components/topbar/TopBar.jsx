97d72ae25aa9134d6e241a013133568864fe8635
samade747 <samad.e747@gmail.com>
2024-04-17 15:06:28 +0500

    306PM 17 4 2024

--- samadfacebook/src/components/topbar/TopBar.jsx
+++ samadfacebook/src/components/topbar/TopBar.jsx
@@ -4,3 +4,15 @@ import { Link, useNavigate } from "react-router-dom";
 import "./topbar.css";
 import { Button, Dropdown } from "antd";
 import { FaBars } from "react-icons/fa";
+
+
+import Pic_1 from "./Person/1.jpg";
+import UserIcon from "./Person/UserIcon";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
+const TopBar = () => {
+    const navigate = useNavigate();
+    const { user, isShow, friends, dispatch } = useContext(AuthContext);
+    
+}
