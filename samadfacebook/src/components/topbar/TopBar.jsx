bc8f7d90fcea5cf22501818f16b7b01774cc3caa
samade747 <samad.e747@gmail.com>
2024-04-18 12:27:59 +0500

    1227

--- samadfacebook/src/components/topbar/TopBar.jsx
+++ samadfacebook/src/components/topbar/TopBar.jsx
@@ -12,7 +12,50 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
 const TopBar = () => {
-    const navigate = useNavigate(); // 
-    const { user, isShow, friends, dispatch } = useContext(AuthContext);
+    const navigate = useNavigate(); // Creates a navagation instance usng react router
+
+    // Uses the AuthContext to retrieve the current user, show state, friends list, and dispatch function
+    const { user, isShow, friends, dispatch } = useContext(AuthContext); 
     
+    console.log("friends", friends);
+
+    // functon to handle user sign-out
+    const signOutHandler = () => {
+        console.log("checking sign out handler")        
+        localStorage.removeItem('user') // removes the user from local storage
+        window.location.reload(); // reload the page
+        navigate("/login") // navigate to login page
+    }
+
+    // function to handle showing and hiding friends list
+    const showFriendsHandler = () => {
+        console.log("checking show friends handler")
+        dispatch({ type: "SHOW_FRIENDS", payload: !isShow }) // dispatch action to show and hide friends list
+        console.log('friends', friends);
+    };
+
+    // Defines menu items for the dropdown
+    const items = [
+        {
+            key: "1",
+            label: (<Link to={'/'}>
+            Home
+            </Link>
+            ),
+        },
+        
+
+
+
+
+
+
+    ]
+
+
+
+
+
+
+
 }
