60d3749e4afe0ff7273eba6efaaf756b029aec96
samade747 <samad.e747@gmail.com>
2024-04-18 14:37:03 +0500

    236pm 18 4 2024

---
+++ samadfacebook/src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+import HomeIcon from '@mui/icons-material/Home';
+import "./home.css";
+import TopBar from '../../components/topbar/TopBar';
+import Sidebar from 'antd/es/layout/Sider';
+import Rightbar from '../../components/rightbar/Rightbar';
+
+
+const Home = () => {
+    
+    return(
+    <>
+    <TopBar />        
+    </>
+      
+    )
+
+}
+
+export default Home;
