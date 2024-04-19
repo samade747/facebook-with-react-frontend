9db335c7179e11fb9a9850898f17057d6fabee0d
samade747 <samad.e747@gmail.com>
2024-04-19 15:11:35 +0500

    Update Profile.jsx

--- samadfacebook/src/pages/profile/Profile.jsx
+++ samadfacebook/src/pages/profile/Profile.jsx
@@ -1,3 +1,4 @@
 import './profile.css';
 import TopBar from '../../components/topbar/TopBar';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Feed from '../../components/rightbar/Rightbar';
