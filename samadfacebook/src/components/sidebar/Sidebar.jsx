49fa3fdea630965e1293e57b3a57f05420aeb1c0
samade747 <samad.e747@gmail.com>
2024-04-19 11:23:58 +0500

    1123am 19 4 2024

--- samadfacebook/src/components/sidebar/Sidebar.jsx
+++ samadfacebook/src/components/sidebar/Sidebar.jsx
@@ -15,18 +15,57 @@ const Sidebar = () => {
         {/* Displays the user's profile picture and name */}
         { isShow &&
          <div className="sidebar">
-            <div>
+            <div className="sidebarWrapper">
+               <ul className="sidebarList">
+                  <li className="sidebarListItem" >
+                     <RssFeed className="sidebarIcon" />
+                     <span className="sidebarListItemText">Feed</span>
+                  </li>
 
+
+                  <li className="sidebarListItem">
+                     <Chat className="sidebarIcon" />
+                     <span className="sidebarListItemText">Chats</span>
+                  </li>
+
+
+                  <li className="sidebarListItem">
+                     <PlayCircleFilledOutline className="sidebarIcon" />
+                     <span className="sidebarListItemText">Videos</span>
+                  </li>
+
+
+                  <li className="sidebarListItem">
+                     <Group className="sidebarIcon" />
+                     <span className="sidebarListItemText">Groups</span>
+                     
+                  </li>
+
+                  <li className="sidebarListItem">
+                     <Group className="sidebarIcon" />
+                     <span className="sidebarListItemText">Bookmarks</span>                                         
+                  </li>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               </ul>  
          </div>
         }
-
-
-
-
-
-
-       </div> 
+            </div> 
     )
 
+   
 }
 
