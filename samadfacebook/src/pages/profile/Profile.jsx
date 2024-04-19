899176979d33ec6c62873377ba2e8d2640f5be8e
samade747 <samad.e747@gmail.com>
2024-04-19 15:18:53 +0500

    318 19

--- samadfacebook/src/pages/profile/Profile.jsx
+++ samadfacebook/src/pages/profile/Profile.jsx
@@ -2,3 +2,5 @@ import './profile.css';
 import TopBar from '../../components/topbar/TopBar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Feed from '../../components/rightbar/Rightbar';
+import Rightbar from '../../components/rightbar/Rightbar';
+import CoverPic from '../../components/coverPic/CoverPic';
